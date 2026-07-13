5d2de3e1b435be89422627e2c936a14ab84d4736
2kmkmkm <02dlrudals@gmail.com>
2026-07-13 12:56:36 +0900

    feat: Implement User Entity, DTO

---
+++ BE/src/main/java/com/menupick/be/user/entity/User.java
@@ -0,0 +1,37 @@
+package com.menupick.be.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int pointBalance = 0;
+
+    @Builder
+    public User(String email, String password, String name, int pointBalance) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.pointBalance = pointBalance;
+    }
+}
